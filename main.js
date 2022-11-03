9c5f9191a98471498ed3ddc756a1344f8ea6955d
Kristian Rahnev <mrchreesa@gmail.com>
2022-11-03 12:48:25 +0000

    big screen size

--- main.js
+++ main.js
@@ -202,8 +202,6 @@ const observer = new IntersectionObserver((entries) => {
     console.log(entry);
     if (entry.isIntersecting) {
       entry.target.classList.add("show");
-    } else {
-      entry.target.classList.remove("show");
     }
   });
 });
